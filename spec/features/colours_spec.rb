d3cfb91aeef32f13a135c3c74250006d086fd2a7
emmpak <eiliev05@yahoo.com>
2017-06-19 15:52:51 +0100

    Add test for query string

--- spec/features/colours_spec.rb
+++ spec/features/colours_spec.rb
@@ -14,4 +14,48 @@ feature 'view colours' do
     visit('/')
     expect(page).to have_selector('a', count: 7)
   end
+
+  context 'when clicked, the color gets passed in the query string' do
+    scenario 'red link' do
+      visit('/')
+      click_link('red')
+      expect(current_path).to eq '/images/red'
+    end
+
+    scenario 'pink link' do
+      visit('/')
+      click_link('pink')
+      expect(current_path).to eq '/images/pink'
+    end
+
+    scenario 'orange link' do
+      visit('/')
+      click_link('orange')
+      expect(current_path).to eq '/images/orange'
+    end
+
+    scenario 'yellow link' do
+      visit('/')
+      click_link('yellow')
+      expect(current_path).to eq '/images/yellow'
+    end
+
+    scenario 'green link' do
+      visit('/')
+      click_link('green')
+      expect(current_path).to eq '/images/green'
+    end
+
+    scenario 'blue link' do
+      visit('/')
+      click_link('blue')
+      expect(current_path).to eq '/images/blue'
+    end
+
+    scenario 'purple link' do
+      visit('/')
+      click_link('purple')
+      expect(current_path).to eq '/images/purple'
+    end
+  end
 end
