79c4c19d86b0fb58bb246df87280c04673def8d2
emmpak <eiliev05@yahoo.com>
2017-06-20 16:10:48 +0100

    Add Term class and randomize class method

---
+++ app/models/term.rb
@@ -0,0 +1,7 @@
+class Term
+  CHOICES = %w(man, mountain, state, ocean, country, building, cat, airline, wealth,  happiness, pride, fear, religion, bird, book, phone, rice, snow, water)
+
+  def self.randomize
+    CHOICES.sample
+  end
+end
--- spec/features/show_images_spec.rb
+++ spec/features/show_images_spec.rb
@@ -1,7 +1,15 @@
 feature 'images' do
-  scenario 'five images are shown' do
+
+  before do
     visit('/')
     click_link('red')
+  end
+
+  scenario 'five images are shown' do
     expect(page).to have_css('img', count: 5)
+  end
+
+  scenario 'one image can be clicked' do
+    within('#top-image') { expect(page).to have_link('a', count: 1) }
   end
 end
