47db8c8fc67934a20f62f974709fd4ed8215c2f0
emmpak <eiliev05@yahoo.com>
2017-06-21 07:17:54 +0100

    Add search history

--- app/app.rb
+++ app/app.rb
@@ -13,14 +13,19 @@ class App < Sinatra::Base
     erb :'colors/index'
   end
 
-  get '/images/:color' do
-    search = Search.new(color: params[:color], term: Term.randomize)
-    uri = URI(search.build)
-    http_response = Net::HTTP.get(uri)
-    search.response = http_response
-    search.save
-    @result = Result.new(http_response)
-    # @result = Result.new(File.read('./spec/sample_api_response.rb'))
+  post '/search/new/:color' do
+    # search = Search.new(color: params[:color], term: Term.randomize)
+    # uri = URI(search.build)
+    # http_response = Net::HTTP.get(uri)
+    # search.response = http_response
+    # search.save
+    redirect '/search'
+  end
+
+  get '/search' do
+    # @result = Result.new(Search.last.response)
+    @result = Result.new(File.read('./spec/sample_api_response.rb'))
+    @previous = Search.all
     erb :'images/index'
   end
 end
